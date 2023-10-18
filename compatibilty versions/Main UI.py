2162078d8fce9656387d7705ad1043ae3a28feed
Macle57 <148278124+Macle57@users.noreply.github.com>
2023-10-18 13:02:22 +0530

    Add files via upload

---
+++ compatibilty versions/Main UI.py
@@ -0,0 +1,396 @@
+from csv_file_func import *
+
+print("You are now viewing the private database which keeps track of all the employees")
+print("This program is going to help you navigate and work with this database")
+print("x-------------------------------------------x\n")
+
+
+# Used to get int input and filter out everything else
+def getint(txt):
+    while True:
+        inte = ''.join(i for i in input(txt) if i.isdigit())
+        if inte == '':
+            print('Please enter valid integers i.e. 1 2 3 etc.')
+        else:
+            return int(inte)
+
+
+# Used to get date in correct format and correct the mistakes of wrong format.
+def getdate(DOB=True):
+    global dob
+    while True:
+        if DOB:
+            print("Enter employee's date of birth (MM/DD/YYYY):   ", end='')
+        else:
+            print("Enter employee's date of joining (MM/DD/YYYY):   ", end='')
+        truedate = ''.join(i for i in input(':\t') if i == '/' or i.isdigit())
+        if truedate == '0':
+            return truedate
+        try:
+            date = managedate(truedate)
+        except:
+            print("Please ensure to use '/' between dates.")
+            continue
+        if len(truedate) != 10:
+            print('Please ensure to add a 0 before a single digit entry, for eg: 11/03/2005')
+        elif date[2] > 31:
+            print('Please enter the date correctly, input the DOB again.')
+        elif date[1] > 12:
+            print('Please enter the month correctly, input the DOB again.')
+        elif DOB:
+            if 2022 - date[0] < 18:
+                print('Please ensure employee is 18+')
+            elif 2022 - date[0] > 60:
+                print("Please ensure employee is less then 60 years old due to company's policy")
+            else:
+                dob = date
+                return truedate
+        else:
+            try:
+                age = date[0] - dob[0]
+            except:
+                age = date[0] - 1975
+            if 2022 - date[0] < 0:
+                print('Please enter a date in past.')
+            elif 61 < age or age < 17:
+                print('Please ensure employee is in between 18-60 years of age.')
+            else:
+                return truedate
+
+
+# Used to get sex inputted and account for many possible answers.
+def getsex():
+    while True:
+        sex = input("Enter employee's sex (M/F/O):  ").upper()
+        i
+        sex = ''.join(i for i in sex if not i.isspace() and i.isalpha() or i == '0')
+        if sex == '0':
+            return sex
+        if sex in ['MALE', 'M', 'MAN', 'BOY']:
+            return 'M'
+        elif sex in ['FEMALE', 'F', 'WOMAN', 'GIRL']:
+            return 'F'
+        elif sex in ['O', 'OTHER', 'OTHERS']:
+            return 'O'
+        else:
+            print('Please enter a valid sex and check your spellings.')
+
+
+# Used to get the employee id in the correct format
+def getemp():
+    while True:
+        emp = getint("Enter employee id:\t")
+        if len(str(emp)) < 4 and emp != 0:
+            print("Please enter employee id of atleast 4 numeric digits")
+        else:
+            return emp
+
+
+# Used to get a record inputted from the user.
+def getinput():
+    while True:
+        inp = [getemp(),  # Employee ID
+               input("Enter employee's full Name:   "),  # Employee Name
+               getdate(),  # DOB
+               getsex(),  # Sex
+               input("Enter employee's email id:   "),  # Email id
+               input("Enter employee's address:   "),  # Address
+               getint("Enter employee's phone number:   "),  # Phone Number
+               getint("Enter employee's salary:   "),  # Employee Salary
+               input("Enter employee's designation:   "),  # Designation
+               getdate(False)]  # Date of joining
+        print("Following entered values will be passed into database."
+              "\n(Don't worry 0 will be replaced with original value)")
+        printer(inp)
+        print("Are you satisfied with entered data?")
+        choice = getchoiceYorN()
+        if choice:
+            return inp
+        else:
+            continue
+
+
+# Used to get simple yes/no inputs from user
+def getchoiceYorN():
+    while True:
+        Choice = input(":    ")
+
+        if Choice.lower() in ('y', 'yes', 'yup', 'oui', 'si'):
+            return True
+        elif Choice.lower() in ('n', 'no', 'nope', 'non'):
+            return False
+        else:
+            print("\nEnter a valid response.\n")
+
+
+# Used to sort the file in multiple ways
+def choose_sort():
+    while True:
+        print("How will you sort the data?"
+              "\nType 1 for Sorting in ascending order or just leave blank."
+              "\nType 2 for Sorting in descending order"
+              "\nType 0 to go back to previous menu")
+        choice = ''.join(i for i in input(":    ") if i.isdigit())
+        if choice == '0':
+            print("Going back.......\n")
+            return None
+        elif choice == '1' or choice == '':
+            return False
+        elif choice == '2':
+            return True
+        else:
+            print("\nEnter a valid response.\n")
+
+
+# Used to get a choice from the user.
+def choose_opr():
+    while True:
+        opr = input("Choose the method of operation.\n"
+                    "Type 0 to exit this menu\n"
+                    "Type 1 for finding equal entries or just leave blank.\n"
+                    "Type 2 for finding greater entries.\n"
+                    "Type 3 for finding lesser entries.\n"
+                    ":  ")
+        opr = ''.join(i for i in opr if i.isdigit())
+        if opr in ['0', '1', '2', '3']:
+            return opr
+        elif opr == '':
+            return '1'
+        else:
+            print('Please enter a valid no. (1-3).')
+
+
+# Used to get the index input from user withing range
+def getind(txt, blank=False):
+    file = filelist(csvfile)
+    maxsno = file[-1][0]
+    minsno = file[1][0]
+    while True:
+        ind = ''.join(i for i in input(txt) if i.isdigit())
+        if ind == '' and blank:
+            return ''
+        elif ind == '' and not blank:
+            return int(ind)
+        elif int(minsno) <= int(ind) <= int(maxsno):
+            return int(ind)
+        else:
+            print(f'Please enter a integer between {minsno}-{maxsno}')
+
+
+# Used to update the records
+def updateUI():
+    print("Enter Serial No. or Employee ID or Name to update record off.")
+    update_meth = input(":    ")
+
+    print("Following entry is going to be updated.")
+    ind = auto_search(csvfile, update_meth)
+    if ind is None:
+        return None
+    print('\nAre you sure? (Y/N)')
+    choice = getchoiceYorN()
+    if choice:
+        print("Enter 0 if you don't want to update certain field.")
+
+        updateSNO(csvfile, getinput(), ind)
+        print('\n')
+        search_sno(csvfile, ind)
+        print('\n')
+    else:
+        print('OK updation canceled.')
+
+
+# Used to delete the records
+def delUI():
+    print("Enter Serial No. or Employee ID or Name to delete record off, leave blank to delete last record.")
+    InpSno = input(":    ")
+    if InpSno.strip(' ') == '':
+        deletion(csvfile)
+        return None
+    print("Following entry/s are going to be deleted: ")
+    ind = auto_search(csvfile, InpSno)
+    if ind is None:
+        return None
+    print("\n Are you sure? (Y/N)")
+    choice = getchoiceYorN()
+    if choice:
+        deletion(csvfile, ind)
+        print("Entry/s has/have been successfully deleted.")
+    else:
+        print('OK deletion cancelled.')
+
+
+# While loop for the menu
+while True:
+    print("\nWhat should I do for you?"
+          "\nType 1 for Inserting data"
+          "\nType 2 for Updating values of the data"
+          "\nType 3 for Delete entries of the data"
+          "\nType 4 for Searching the data"
+          "\nType 5 for Viewing sorted data"
+          "\nType 6 for Returning a report of the Company"
+          "\nType 7 for Displaying the entire file at once."
+          "\nType 0 for Exiting the program")
+    MainInp = getint(":    ")
+    if MainInp == 0:
+        print("Exiting the program. Goodbye!")
+        break
+    if MainInp == 1:  # insertion
+        print("Start entering the details of the employee one by one.")
+        insert(csvfile,
+               getinput(),
+               getind('Enter the index no. u want to add the record to, '  # Index
+                      'leave blank if you wish to add at the end: ', True))
+        print('\nInsertion Complete\n')
+
+
+    elif MainInp == 2:  # update
+        updateUI()
+
+    elif MainInp == 3:  # delete
+        delUI()
+
+    elif MainInp == 4:  # search
+        while True:
+            print("How would you like to search the data?"
+                  "\nType 1 for Searching by Serial Number"
+                  "\nType 2 for Searching by Employee's Name"
+                  "\nType 3 for Searching by Employee ID"
+                  "\nType 4 for Searching by Salary"
+                  "\nType 5 for Searching by Designation"
+                  "\nType 6 for Searching by Date of Joining"
+                  "\nType 7 for Searching by Sex"
+                  "\nType 8 to automatically detect Sno., Employee ID or Name."
+                  "\nType 0 to go back to previous menu")
+            SecInp = getint(":    ")
+
+            if SecInp == 0:
+                print("Going back.......\n")
+                break
+
+            elif SecInp == 1:
+                opr = choose_opr()
+                if opr == '0':
+                    print("Going back.......\n")
+                    break
+                search_sno(csvfile,
+                           getind("Enter a serial number or leave blank to check last entry:   "),
+                           opr)
+                break
+
+            ###---------------------Name--------------------------
+            elif SecInp == 2:
+                better_srch_name(csvfile, input("Enter a first/last/full name:   "))
+                break
+            ###---------------------EmpId--------------------------
+            elif SecInp == 3:
+                search_empid(csvfile, getemp())
+                break
+            ###---------------------Salary--------------------------
+            elif SecInp == 4:
+                opr = choose_opr()
+                if opr == '0':
+                    print("Going back.......\n")
+                    break
+                search_salary(csvfile,
+                              getint("Enter the Amount:   "), opr)
+                break
+            ###---------------------Designation--------------------------
+            elif SecInp == 5:
+                search_divion(csvfile, input('Enter the division:   '))
+                break
+            ###---------------------DOJ40--------------------------
+            elif SecInp == 6:
+                opr = choose_opr()
+                if opr == '0':
+                    print("Going back.......\n")
+                    break
+                srch_date(csvfile,
+                          getdate(False), opr)
+                break
+
+            ###---------------------Sex--------------------------
+            elif SecInp == 7:
+                search_sex(csvfile, getsex())
+                break
+            elif SecInp == 8:
+                auto_search(csvfile, input("Enter any Sno., Employee ID or Name\n"
+                                           ":   "))
+            ###---------------------Valid Respone--------------------------
+            else:
+                print("\nEnter a valid response.\n")
+
+    ##--Sorted function Start, updates the file only if users wants to.
+    elif MainInp == 5:
+        while True:
+            print("Choose your method of sorting\n"
+                  "Type 1 for Sorting by employee id\n"
+                  "Type 2 for Sorting by name\n"
+                  "Type 3 for Sorting by salary\n"
+                  "Type 4 for Sorting by date of joining\n"
+
+                  "Type 0 to go back to previous menu")
+            SecInp = getint(":    ")
+
+            ###---------------------Going Back--------------------------
+            if SecInp == 0:
+                print("Going back.......\n")
+                break
+            ###---------------------EmpID--------------------------
+            if SecInp in [1, 2, 3, 4]:
+                choice = choose_sort()
+                if choice is None:
+                    break
+            else:
+                print("\nEnter a valid response.\n")
+                continue
+
+            ###---------------------Emp_ID--------------------------
+            if SecInp == 1:
+                data = sort_empid(csvfile, choice)
+
+            ###---------------------Name--------------------------
+            elif SecInp == 2:
+                data = sortalpha(csvfile, choice)
+
+            ###---------------------Salary--------------------------
+            elif SecInp == 3:
+                data = sort_salary(csvfile, choice)
+
+            ###---------------------Date--------------------------
+            elif SecInp == 4:
+                data = sort_date(csvfile, choice)
+            print("Would you like to update the file with the sorted data?")
+            if getchoiceYorN():
+                writelist_to_file(data)
+                break
+            else:
+                break
+
+
+
+    # --Report function Start
+    elif MainInp == 6:
+        print("\nWhich report should I present to you?\n"
+              "Type 1 for getting a report on company's expenditure on Salary\n"
+              "Type 2 for getting a report on company's work division\n"
+              "Type 0 to go back to previous menu")
+        while True:
+            SecInp = getint(":    ")
+            ###---------------------Going Back--------------------------
+            if SecInp == 0:
+                print("Going back.......\n")
+                break
+
+            elif SecInp == 1:
+                better_salary_report(csvfile)
+                break
+            elif SecInp == 2:
+                better_rep_div_cnt(csvfile)
+                break
+            else:
+                print('Please enter a valid response')
+    elif MainInp == 7:
+        display(csvfile)
+
+    else:
+        print("\nEnter a valid response.\n")
---
+++ csv_file_func.py
@@ -0,0 +1,415 @@
+# from __future__ import annotations                    #uncomment this line if you're between 3.7-3.10
+
+from statistics import mode
+from csv import reader, writer
+
+csvfile = "MOCK_DATA.csv"
+
+_Sno_ = 0
+_Name_ = 2
+_DOB_ = 3
+_Salary_ = 8
+_Address_ = 6
+_EmpID_ = 1
+_Sector_ = 9
+_Sex_ = 4
+_email_ = 5
+_DOJ_ = 10
+
+
+# Used to print formatted records and automatically account for increased records.
+def printer(i: list):
+    try:
+        print('{:<7}{:<10}{:<20}{:<12}{:<5}{:<35}{:<30}{:<15}{:<10}{:<25}{:<15}'.format(*i))
+    except IndexError:
+        print(('{:<15}' * len(i)).format(*i))
+
+
+# Used to get the file as a list
+def filelist(file) -> list:
+    with open(file) as temp:
+        return list(reader(temp, delimiter=','))
+
+
+# Writes a list to the file as a new record
+def writelist_to_file(cont: list, rd: str = 'w'):
+    with open(csvfile, rd, newline='') as temp:
+        writer(temp, delimiter=',').writerows(cont)
+
+
+def display(file):  # Used to display the file
+    for i in filelist(file):
+        printer(i)
+
+
+def displaylist(temp: list):  # Display list in file format.
+    for i in temp:
+        printer(i)
+
+
+# Converts date to yyyy-mm-dd from string
+def managedate(date: str) -> list:  # input date- mm/dd/yyyy
+    date = list(int(j) for j in date.split('/', 2))
+    return [date[2], date[0], date[1]]  # YYYY-MM-DD
+
+
+# Correct serial number of the file in list format if needed
+def modifysnolist(var: list) -> list:
+    cnt = 0
+    for i in var:
+        cnt += 1
+        i[_Sno_] = cnt
+    return var
+
+
+# Inserts a record at desired position, and at last if none mentioned
+def insert(file, cont: list, ind=''):
+    tempstor = filelist(file)
+    if ind == '':
+        index = int(tempstor[-1][_Sno_]) + 1
+        writelist_to_file([[index, *cont]], 'a')
+    else:
+        try:
+            tempstor.insert(int(ind), [ind] + cont)
+            header = tempstor[0]
+            tempstor = modifysnolist(tempstor[1::])
+            writelist_to_file([header, *tempstor])
+        except ValueError:
+            print('Please enter a serial no. as a integer')
+
+
+# Updates the desired record
+def updateSNO(file, cont: list, sno: int):
+    tempstor = filelist(file)
+    record = tempstor[sno][1::]
+    for i in range(len(cont)):
+        if str(cont[i]) == '0':
+            cont[i] = record[i]
+
+    tempstor[sno] = [sno] + cont
+    writelist_to_file(tempstor)
+
+
+# Deletes a record
+def deletion(file, ind=None):
+    tempstor = filelist(file)
+    header = tempstor[0]
+    if ind is None:
+        print("Successfully deleted record: ")
+        printer(header)
+        printer(tempstor.pop())
+        tempstor.remove(header)
+    else:
+        flag = True
+        for i in tempstor:
+            if i[_Sno_] == str(ind):
+                print("Successfully deleted record: ")
+                printer(header)
+                printer(i)
+                tempstor.remove(i)
+                tempstor = modifysnolist(tempstor[1::])
+                flag = False
+                break
+        if flag:
+            print(f'Cant find a record with mentioned serial number:{ind}')
+
+    writelist_to_file([header, *tempstor])
+
+
+# Searches a record with sno
+def search_sno(file, sno: int, opr='1') -> int | None:
+    tempstor = filelist(file)
+    header = tempstor[0]
+    flag = True
+    if opr == "1":
+        for i in tempstor[1::]:
+            if int(i[_Sno_]) == sno:
+                printer(header)
+                printer(i)
+                return int(i[int(_Sno_)])
+        return None
+
+    elif opr == "2":
+        for i in tempstor[1::]:
+            if int(i[_Sno_]) > sno:
+                if flag:
+                    printer(header)
+                    flag = False
+                printer(i)
+
+    elif opr == "3":
+        for i in tempstor[1::]:
+            if int(i[_Sno_]) < sno:
+                if flag:
+                    printer(header)
+                    flag = False
+                printer(i)
+    else:
+        print('Please enter a reasonable operator with respect to ur input.')
+
+
+# Searches the record smartly with any last name or first name automatically
+def better_srch_name(file, name: str) -> int:
+    tempstor = filelist(file)
+    names = []
+    for i in tempstor:
+        names.append(i[_Name_].lower().split())
+    flag = False
+    flag1 = True
+    for i in names:
+        if name.lower() in i or name.lower() == ' '.join(i):
+            indx: int = names.index(i)
+            if flag1:
+                printer(tempstor[0])
+                flag1 = False
+            printer(tempstor[indx])
+            flag = True
+    if flag:
+        return indx
+    else:
+        print('Check your spelling.')
+
+
+# Searches with emp id
+def search_empid(file, parameter_empid: int) -> int | None:
+    tempstor = filelist(file)
+    for i in tempstor[1::]:
+        if int(i[_EmpID_]) == parameter_empid:
+            printer(tempstor[0])
+            printer(i)
+            return int(i[_Sno_])
+    print("\n Can't find a employee with mentioned employee id.")
+    return None
+
+
+# automatically detects input from name, Sno., empid and search.
+def auto_search(file, inp: int | str) -> int:
+    try:
+        inp = int(inp)
+        if len(str(inp)) <= 3:  # Sno. checker
+            ind = search_sno(file, inp)
+            if ind is None:
+                print('Cant find someone with mentioned Sno.')
+            return ind
+        else:  # Emp ID assumer
+            ind = search_empid(file, inp)
+            if ind is None:
+                print('Cant find someone with mentioned Employee ID.')
+            return ind
+    except ValueError:  # Name
+        ind = better_srch_name(file, inp)
+        if ind is None:
+            print('Cant find someone with mentioned Name.')
+        return ind
+
+
+# Searches with given SEX of employee
+def search_sex(file, sex: str):
+    tempstor = filelist(file)
+    flag = True
+    for i in tempstor[1::]:
+        if sex == i[_Sex_]:
+            printer(i)
+            flag = False
+        elif sex == i[_Sex_]:
+            printer(i)
+            flag = False
+        elif sex == i[_Sex_]:
+            printer(i)
+            flag = False
+    if flag:
+        print('Please enter a valid sex(M/F/O)')
+
+
+# Searches with mentioned salary
+def search_salary(file, parameter_salary: int, operator: str = '1'):
+    tempstor = filelist(file)
+    header = tempstor[0]
+    flag = True
+    flag1 = True
+    for i in tempstor[1::]:
+        if operator == "1":
+            if int(i[_Salary_]) == parameter_salary:
+                if flag1:
+                    printer(header)
+                    flag1 = False
+                printer(i)
+                flag = False
+        elif operator == "2":
+            if int(i[_Salary_]) > parameter_salary:
+                if flag1:
+                    printer(header)
+                    flag1 = False
+                printer(i)
+                flag = False
+        elif operator == "3":
+            if int(i[_Salary_]) < parameter_salary:
+                if flag1:
+                    printer(header)
+                    flag1 = False
+                printer(i)
+                flag = False
+
+    if flag:
+        print('Please enter a reasonable operator.')
+
+
+# Search with dates
+def srch_date(file, inp_date: str, opr="1"):
+    tempstor = filelist(file)
+    header = tempstor[0]
+    date = managedate(inp_date)
+    flag = True
+    flag1 = False
+    for i in tempstor[1::]:
+        srtdate = managedate(i[_DOJ_])
+        if str(opr) == '1':
+            if srtdate == date:
+                if flag1:
+                    printer(header)
+                    flag1 = False
+                printer(i)
+                flag = False
+        elif str(opr) == '2':
+            if srtdate > date:
+                if flag1:
+                    printer(header)
+                    flag1 = False
+                printer(i)
+                flag = False
+        elif str(opr) == '3':
+            if srtdate < date:
+                if flag1:
+                    printer(header)
+                    flag1 = False
+                printer(i)
+                flag = False
+    if flag:
+        print('Enter a reasonable date and ensure its in correct format.')
+
+
+# Search by division
+def search_divion(file, parameter_division: str):
+    tempstor = filelist(file)
+    flag1 = True
+    for row in tempstor:
+        if row[_Sector_].lower() == parameter_division.lower().rstrip(' '):
+            if flag1:
+                printer(tempstor[0])
+                flag1 = False
+            printer(row)
+
+
+# Sorts alphabetically
+def sortalpha(file, desc=False):
+    tempstor = filelist(file)
+
+    def function(x: list) -> str:
+        return x[_Name_]
+
+    header = tempstor[0]
+    tempstor.remove(header)
+    tempstor = modifysnolist(sorted(tempstor, key=function, reverse=desc))
+    sorteddata = [header, *tempstor]
+    displaylist(sorteddata)
+    return sorteddata
+
+
+# Sorts by date
+def sort_date(file, desc=False) -> list:
+    tempstor = filelist(file)
+
+    def function(x: list) -> list:
+        return managedate(x[_DOJ_])
+
+    header = tempstor[0]
+    tempstor.remove(header)
+    tempstor = modifysnolist(sorted(tempstor, key=function, reverse=desc))
+    sorteddata = [header, *tempstor]
+    displaylist(sorteddata)
+    return sorteddata
+
+
+# Sorts by salary
+def sort_salary(file, desc=False):
+    tempstor = filelist(file)
+
+    header = tempstor[0]
+    tempstor.remove(header)
+
+    def function(x: list) -> int:
+        return int(x[_Salary_])
+
+    tempstor = modifysnolist(sorted(tempstor, key=function, reverse=desc))
+    sorteddata = [header, *tempstor]
+    displaylist(sorteddata)
+    return sorteddata
+
+
+# Sorts by employee ID
+def sort_empid(file, desc=False):
+    tempstor = filelist(file)
+
+    header = tempstor[0]
+    tempstor.remove(header)
+
+    def function(x: list) -> int:
+        return int(x[_EmpID_])
+
+    tempstor = sorted(tempstor, key=function, reverse=desc)
+
+    tempstor = modifysnolist(tempstor)
+    sorteddata = [header, *tempstor]
+    displaylist(sorteddata)
+    return sorteddata
+
+
+# Makes a salary report
+def better_salary_report(file):
+    tempstor = filelist(file)
+    sal = []
+    for i in tempstor[1::]:
+        sal.append(int(i[_Salary_]))
+    print("SALARY REPORT"
+          "\n-------------\n"
+          f"Total Paid Salary:  {sum(sal)}"
+          "\n <Sal> (<Name>: <emp_id>)\n"
+          f"Average Salary Paid:\t{sum(sal) // len(sal)}\n"
+          f"Highest Salary:\t{max(sal)} ({tempstor[sal.index(max(sal))][_Name_]} :"
+          f"{tempstor[sal.index(max(sal))][_EmpID_]} )\n"
+          f"Lowest Salary:\t{min(sal)}({tempstor[sal.index(min(sal))][_Name_]}:"
+          f"{tempstor[sal.index(min(sal))][_EmpID_]})")
+
+
+# Makes a report that displays divisions and employees that work in it
+def better_rep_div_cnt(file):
+    print("WORK DIVISON REPORT")
+    print("-------------\n")
+
+    tempstor = filelist(file)
+    tempstor.remove(tempstor[0])
+    print(f"Total Employee Count:   {int(tempstor[-1][_Sno_])} \n")
+    for i in tempstor:
+        i[_Sector_] = i[_Sector_].rstrip(' ')
+    div = []
+    for i in tempstor:
+        div.append(i[_Sector_])
+    l = div.count(mode(div))
+    div = sorted(list(set(div)))
+    print(('{:<25}' * len(div)).format(*(list(i.upper() for i in div))))
+    data = list([i[_Name_], i[_Sector_]] for i in tempstor)
+
+    def function(x: list) -> str:
+        return x[1]
+
+    data = sorted(data, key=function)
+    test = []
+    for j in range(l):
+        TEST = [""] * len(div)
+        keye = []
+        for i in range(len(data)):
+            if data[i][1] not in keye and data[i][0] not in test:
+                keye.append(data[i][1])
+                TEST[div.index(data[i][1])] = data[i][0]
+                test.append(data[i][0])
+        print(('{:<25}' * len(div)).format(*TEST))
